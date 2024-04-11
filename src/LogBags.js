a8be14e3bc205df9502c336232cedad16fb0b67f
jacobmolson51002 <jacob@promotition>
2024-04-10 20:17:13 -0500

    added favicon, fixed location issue

--- src/LogBags.js
+++ src/LogBags.js
@@ -1,25 +1,25 @@
-import { useState, useRef } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import "./App.css";
 import { TailSpin } from 'react-loading-icons';
 import MenuIcon from '@mui/icons-material/Menu';
 import { firestore } from "./firebase";
 import { GeoPoint } from 'firebase/firestore';
 import { collection, addDoc, query, getDocs, deleteDoc, limit, orderBy, where } from "@firebase/firestore";
-import { useGeolocated } from "react-geolocated";
 
 export const LogBags = ({ authCode }) => {
   const [authLoading, setAuthLoading] = useState(false);
   const [isChecked, setChecked] = useState(false);
   const [numBags, setnumBags] = useState('');
   const [zoneNum, setzoneNum] = useState('');
+  const [newLocation, setNewLocation] = useState(0);
+  const [coords, setCoords] = useState(null)
 
-  const { coords, isGeolocationAvailable, isGeolocationEnabled } =
-  useGeolocated({
-      positionOptions: {
-          enableHighAccuracy: false,
-      },
-      userDecisionTimeout: 10000,
-  });
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      setCoords({longitude: position.coords.longitude, latitude: position.coords.latitude});
+    })
+  },[])
+
 
   const bagRef = useRef();
   const zoneRef = useRef();
@@ -58,42 +58,49 @@ export const LogBags = ({ authCode }) => {
 
   const logBags = () => {
   if (bagRef.current.value !== "" && zoneRef.current.value !== "" && Number.isInteger(parseInt(bagRef.current.value)) && parseInt(bagRef.current.value) > 0 && Number.isInteger(parseInt(zoneRef.current.value)) && parseInt(zoneRef.current.value) > 0) {
+    let latitude;
+    let longitude;
+    navigator.geolocation.getCurrentPosition((position) => {
+      latitude = position.coords.latitude
+      longitude =  position.coords.longitude
+      setCoords({latitude: latitude, longitude: longitude})
+    })  
     setAuthLoading(true);
-    setTimeout(() => {
-      alert(`Bag ${bagRef.current.value} has been added to Zone ${zoneRef.current.value}`);
-      
-      let data = {
-        bags: parseInt(bagRef.current.value),
-        zone: parseInt(zoneRef.current.value),
-        location: new GeoPoint(coords?.latitude, coords?.longitude),
-        time: new Date(),
-        correction: isChecked,
-        authCode: authCode
-      };
-
-      try {
-        addDoc(ref, data).then(() => {
-          if (isChecked) {
-            const queryRef = query(ref, orderBy('time', 'desc'), where("authCode", "==", parseInt(authCode)), limit(2));  
-            getDocs(queryRef).then((querySnapshot) => {
-                if (querySnapshot.size > 1) { 
-                  const documents = querySnapshot.docs;
-                  const docToDelete = documents[1]; 
-                  deleteDoc(docToDelete.ref).then(() => console.log("Entry was replaced successfully")).catch((error) => console.log("Error replacing entry: ", error));
-                } else {
-                  console.log("No entry to delete or only one entry found.");
-                }
-              }).catch((error) => console.log("Error accessing entry: ", error));
-          }
-        });
-      } catch (e) {
-        console.log(e)
-      }
-
-      bagRef.current.value = '';
-      setAuthLoading(false);
-      setChecked(false);
-      }, 500);
+      setTimeout(() => {
+        alert(`Bag ${bagRef.current.value} has been added to Zone ${zoneRef.current.value}`);
+        
+        let data = {
+          bags: parseInt(bagRef.current.value),
+          zone: parseInt(zoneRef.current.value),
+          location: new GeoPoint(latitude, longitude),
+          time: new Date(),
+          correction: isChecked,
+          authCode: authCode
+        };
+  
+        try {
+          addDoc(ref, data).then(() => {
+            if (isChecked) {
+              const queryRef = query(ref, orderBy('time', 'desc'), where("authCode", "==", parseInt(authCode)), limit(2));  
+              getDocs(queryRef).then((querySnapshot) => {
+                  if (querySnapshot.size > 1) { 
+                    const documents = querySnapshot.docs;
+                    const docToDelete = documents[1]; 
+                    deleteDoc(docToDelete.ref).then(() => console.log("Entry was replaced successfully")).catch((error) => console.log("Error replacing entry: ", error));
+                  } else {
+                    console.log("No entry to delete or only one entry found.");
+                  }
+                }).catch((error) => console.log("Error accessing entry: ", error));
+            }
+          });
+        } catch (e) {
+          console.log(e)
+        }
+  
+        bagRef.current.value = '';
+        setAuthLoading(false);
+        setChecked(false);
+        }, 500);
     } else {
       alert("Please enter all fields and make sure they are valid integers.");
     }
@@ -101,15 +108,7 @@ export const LogBags = ({ authCode }) => {
 
   return (
     <div>
-    {!isGeolocationAvailable ? (
-        <div className="flex flex-col items-center justify-center w-full h-screen">
-            <p className="w-4/5 text-center ">Your browser does not support geolocation</p>
-        </div>
-    ) : !isGeolocationEnabled ? (
-        <div className="flex flex-col items-center justify-center w-full h-screen">
-            <p className="w-4/5 text-center ">Please enable geolocation on your browser's settings, or use a different device and allow location permissions.</p>
-        </div>
-    ) : coords ? (
+    {coords?.latitude ? (
         <div>
             <div className="flex flex-row items-center justify-between w-full">
             <div className="w-2/5 h-2/5" >
