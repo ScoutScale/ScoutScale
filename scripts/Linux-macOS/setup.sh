cbb3029aba751a5410f81412ec9d28d4fd967136
Timothy Sallwasser <tjsntr@mst.edu>
2024-04-15 18:21:20 -0500

    rebased branch in preparation for merge

--- scripts/Linux-macOS/setup.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# This script installs python dependencies listed in requirements.txt
-python3 -m pip install -r ../../scale_UI/requirements.txt
--- scripts/Windows/setup.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-:: This script installs python dependencies listed in requirements.txt
-pip install -r ../../scale_UI/requirements.txt
