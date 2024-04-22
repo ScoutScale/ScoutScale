ea0d5fac69c7d5646336f93e3710e272680684e5
Timothy Sallwasser <tjsntr@mst.edu>
2024-04-22 12:13:21 -0500

    updated scripts to avoid issues with serial package

--- scale_software/scripts/linux-macOS/setup.sh
+++ scale_software/scripts/linux-macOS/setup.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # This script installs python dependencies listed in requirements.txt
+python3 -m pip uninstall serial pyserial -y
 python3 -m pip install -r ../../scale_UI/requirements.txt
--- scale_software/scripts/windows/setup.bat
+++ scale_software/scripts/windows/setup.bat
@@ -1,3 +1,4 @@
 @echo off
 :: This script installs python dependencies listed in requirements.txt
-pip install -r ../../scale_UI/requirements.txt
+python -m pip uninstall serial pyserial -y
+python -m pip install -r ../../scale_UI/requirements.txt
